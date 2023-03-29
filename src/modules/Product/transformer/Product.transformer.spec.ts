a3fb3c11295e38a593c275d45b14c91bedc363b7
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-03-29 14:45:13 +0500

    Update : tests for product transformer

--- src/modules/Product/transformer/Product.transformer.spec.ts
+++ src/modules/Product/transformer/Product.transformer.spec.ts
@@ -1,8 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductTransformer } from './Product.transformer';
-import { mockProductsList } from '../../../../test/mocks/products';
+import {
+  expectedTransformedProductsList,
+  mockProductsList,
+} from '../../../../test/mocks/products';
 import { ProductMediaTransformer } from '../services/productMedia/Product.media.transformer';
 import { ProductVariantTransformer } from '../services/productVariant/Product.variant.transformer';
+import { ProductVariantService } from '../services/productVariant/Product.variants.service';
+import { ProductVariantDestinationService } from 'src/graphql/destination/handlers/productVariant';
 
 describe('This tests whether product transformer is working properly', () => {
   let service: ProductTransformer;
@@ -13,17 +18,20 @@ describe('This tests whether product transformer is working properly', () => {
         ProductTransformer,
         ProductMediaTransformer,
         ProductVariantTransformer,
+        ProductVariantService,
+        ProductVariantDestinationService,
       ],
     }).compile();
 
     service = module.get<ProductTransformer>(ProductTransformer);
   });
 
-  describe('root 2', () => {
-    it('should return "Hello World! 2"', () => {
+  describe('product transformer tests', () => {
+    it('complete product transformation is working fine, this tests using multiple products with different input variations"', () => {
       const data = service.payloadBuilder(mockProductsList);
       console.dir(data, { depth: null });
       expect(data).toBeDefined();
+      expect(data).toStrictEqual(expectedTransformedProductsList);
     });
   });
 });
--- test/mocks/products.ts
+++ test/mocks/products.ts
@@ -972,3 +972,638 @@ export const mockProductsList = {
     },
   ],
 };
+export const expectedTransformedProductsList = [
+  {
+    name: '001',
+    sourceId: 'UHJvZHVjdDoxMjUxNQ==',
+    description:
+      '{"time": 1662995227870, "blocks": [{"id": "cqWmV3MIPH", "data": {"text": "FACE MASK AB RHINESTONES Size : One Size 90% Polyester, 10% Spandex, Comfortable 3D Fitment, Reuseable, Washable Neonprene (Irritation Free), AB Stones, No returns, Not Recommended for children under 3, Non-medical mask "}, "type": "paragraph"}], "version": "2.24.3"}',
+    categoryId: 'Q2F0ZWdvcnk6MTU=',
+    styleNumber: '001',
+    vendorDetails: { vendorId: '610', vendorName: '2 Of A Kind Accessories' },
+    mediaUrls: [
+      {
+        image: '202007/Z/94810af2-c621-11ea-89ed-002590aaee66_Z.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: '202007/Z/4be6b48e-d19a-11ea-8db9-002590aaee66_Z.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: '202007/Z/54bd19d6-d19a-11ea-8db9-002590aaee66_Z.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: '202007/Z/606702b0-d19a-11ea-8db9-002590aaee66_Z.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: '202007/Z/711fa738-d19a-11ea-8db9-002590aaee66_Z.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: '202007/Z/1cb67470-d1a8-11ea-8dbb-002590aaee66_Z.jpeg',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202007/0896e4a2-d1a8-11ea-8dbb-002590aaee66.jpeg',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202007/a10ab3ea-d1a7-11ea-8dbb-002590aaee66.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202007/b042a9c6-d1a7-11ea-8dbb-002590aaee66.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202007/cae88066-d1a7-11ea-8dbb-002590aaee66.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202007/86bec7d8-d1a7-11ea-8dbb-002590aaee66.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+    ],
+    variantMedia: [
+      {
+        sku: '001-BLA-O-16796618460',
+        url: 'ColorSwatch/202007/0896e4a2-d1a8-11ea-8dbb-002590aaee66.jpeg',
+      },
+      {
+        sku: '001-WHI-O-16796618461',
+        url: 'ColorSwatch/202007/a10ab3ea-d1a7-11ea-8dbb-002590aaee66.JPG',
+      },
+      {
+        sku: '001-PIN-O-16796618462',
+        url: 'ColorSwatch/202007/b042a9c6-d1a7-11ea-8dbb-002590aaee66.JPG',
+      },
+      {
+        sku: '001-GRE-O-16796618463',
+        url: 'ColorSwatch/202007/cae88066-d1a7-11ea-8dbb-002590aaee66.JPG',
+      },
+      {
+        sku: '001-BLU-O-16796618464',
+        url: 'ColorSwatch/202007/86bec7d8-d1a7-11ea-8dbb-002590aaee66.JPG',
+      },
+    ],
+    variants: [
+      {
+        color: 'BLACK',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '3.5',
+        sku: '001-BLA-O-16796618460',
+        resalePrice: '5.6',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+      {
+        color: 'WHITE',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '3.5',
+        sku: '001-WHI-O-16796618461',
+        resalePrice: '5.6',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+      {
+        color: 'PINK',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '3.5',
+        sku: '001-PIN-O-16796618462',
+        resalePrice: '5.6',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+      {
+        color: 'GREY',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '3.5',
+        sku: '001-GRE-O-16796618463',
+        resalePrice: '5.6',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+      {
+        color: 'BLUE',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '3.5',
+        sku: '001-BLU-O-16796618464',
+        resalePrice: '5.6',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: '004',
+    sourceId: 'UHJvZHVjdDoxMjExMA==',
+    description:
+      '{"time": 1662995227870, "blocks": [{"id": "cqWmV3MIPH", "data": {"text": "Cotton Lining Adjustable Ear Strap Square Sequin Fashion Mask Non Medical Use  Not Attended For Children Under 3 "}, "type": "paragraph"}], "version": "2.24.3"}',
+    categoryId: 'Q2F0ZWdvcnk6MTU=',
+    styleNumber: '004',
+    vendorDetails: { vendorId: '610', vendorName: '2 Of A Kind Accessories' },
+    mediaUrls: [
+      {
+        image: '202007/Z/2481a5eb-d1c5-11ea-8dc9-002590aaee66_Z.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202007/223a06a8-d1c8-11ea-8dcb-002590aaee66.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+    ],
+    variantMedia: [
+      {
+        sku: '004-SIL-O-16796598230',
+        url: 'ColorSwatch/202007/223a06a8-d1c8-11ea-8dcb-002590aaee66.JPG',
+      },
+    ],
+    variants: [
+      {
+        color: 'SILVER',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '5.5',
+        sku: '004-SIL-O-16796598230',
+        resalePrice: '8.8',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: '006',
+    sourceId: 'UHJvZHVjdDoxMjA2OA==',
+    description:
+      '{"time": 1662995227870, "blocks": [{"id": "cqWmV3MIPH", "data": {"text": "Filtered Mask   Cotton Lining Adjustable Ear Strap Mask Does Not Come With Filter  Non Medical Mask  Not Recommended For Children Under 3  "}, "type": "paragraph"}], "version": "2.24.3"}',
+    categoryId: 'Q2F0ZWdvcnk6MTU=',
+    styleNumber: '006',
+    vendorDetails: { vendorId: '610', vendorName: '2 Of A Kind Accessories' },
+    mediaUrls: [
+      {
+        image: '202008/Z/97f888e8-dbbd-11ea-90ae-002590aaee66_Z.png',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202008/7c85de24-dcbe-11ea-90fb-002590aaee66.png',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+    ],
+    variantMedia: [
+      {
+        sku: '006-MUL-O-16796597280',
+        url: 'ColorSwatch/202008/7c85de24-dcbe-11ea-90fb-002590aaee66.png',
+      },
+    ],
+    variants: [
+      {
+        color: 'MULTI',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '4',
+        sku: '006-MUL-O-16796597280',
+        resalePrice: '6.4',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: '007',
+    sourceId: 'UHJvZHVjdDoxMjc4MA==',
+    description:
+      '{"time": 1662995227870, "blocks": [{"id": "cqWmV3MIPH", "data": {"text": "Rhinetone Filtered Mask   Cotton Lining Adjustable Ear Strap Mask Comes With Filter  Non Medical Mask  Not Recommended For Children Under 3  "}, "type": "paragraph"}], "version": "2.24.3"}',
+    categoryId: 'Q2F0ZWdvcnk6MTU=',
+    styleNumber: '007',
+    vendorDetails: { vendorId: '610', vendorName: '2 Of A Kind Accessories' },
+    mediaUrls: [
+      {
+        image: '202008/Z/00bfab78-dc44-11ea-90e0-002590aaee66_Z.png',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202008/9d98c3a8-dc44-11ea-90e0-002590aaee66.png',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+    ],
+    variantMedia: [
+      {
+        sku: '007-BLU-O-16796625200',
+        url: 'ColorSwatch/202008/9d98c3a8-dc44-11ea-90e0-002590aaee66.png',
+      },
+    ],
+    variants: [
+      {
+        color: 'BLUE',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '6.75',
+        sku: '007-BLU-O-16796625200',
+        resalePrice: '10.8',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: '010',
+    sourceId: 'UHJvZHVjdDoxMjA1Mw==',
+    description:
+      '{"time": 1662995227870, "blocks": [{"id": "cqWmV3MIPH", "data": {"text": "Size : One Size 90% Polyester, 10% Spandex, Comfortable 3D Fitment, Reuseable, Washable Neonprene (Irritation Free), No returns, Not Recommended for children under 3, Non-medical mask "}, "type": "paragraph"}], "version": "2.24.3"}',
+    categoryId: 'Q2F0ZWdvcnk6MTU=',
+    styleNumber: '010',
+    vendorDetails: { vendorId: '610', vendorName: '2 Of A Kind Accessories' },
+    mediaUrls: [
+      {
+        image: '202008/Z/eadded7a-dc51-11ea-90e1-002590aaee66_Z.png',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202008/f670e53a-dcbe-11ea-90fc-002590aaee66.JPG',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+    ],
+    variantMedia: [
+      {
+        sku: '010-BLA-O-16796597120',
+        url: 'ColorSwatch/202008/f670e53a-dcbe-11ea-90fc-002590aaee66.JPG',
+      },
+    ],
+    variants: [
+      {
+        color: 'BLACK',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '3.5',
+        sku: '010-BLA-O-16796597120',
+        resalePrice: '5.6',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: '020',
+    sourceId: 'UHJvZHVjdDoxMjEzNg==',
+    description:
+      '{"time": 1662995227870, "blocks": [{"id": "cqWmV3MIPH", "data": {"text": "Glitter Mask With Valve  Cotton Lining Adjustable Ear Strap Non Medical Mask  Not Recommended For Children Under 3  "}, "type": "paragraph"}], "version": "2.24.3"}',
+    categoryId: 'Q2F0ZWdvcnk6MTU=',
+    styleNumber: '020',
+    vendorDetails: { vendorId: '610', vendorName: '2 Of A Kind Accessories' },
+    mediaUrls: [
+      {
+        image: '202008/Z/d497e1de-dca0-11ea-90f2-002590aaee66_Z.png',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202008/e2b665ba-dca0-11ea-90f2-002590aaee66.png',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+    ],
+    variantMedia: [
+      {
+        sku: '020-MUL-O-16796598410',
+        url: 'ColorSwatch/202008/e2b665ba-dca0-11ea-90f2-002590aaee66.png',
+      },
+    ],
+    variants: [
+      {
+        color: 'MULTI',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '6.5',
+        sku: '020-MUL-O-16796598410',
+        resalePrice: '10.4',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: '026',
+    sourceId: 'UHJvZHVjdDoxMjQ5Mg==',
+    description:
+      '{"time": 1662995227870, "blocks": [{"id": "cqWmV3MIPH", "data": {"text": "Black Face Mask W/Clear Stones  Comfortable 3D Fitment Reuseable, Washable Neonprene (Irritation Free),  Clear Stones No returns Not Recommended for children under 3 Non-medical mask "}, "type": "paragraph"}], "version": "2.24.3"}',
+    categoryId: 'Q2F0ZWdvcnk6MTU=',
+    styleNumber: '026',
+    vendorDetails: { vendorId: '610', vendorName: '2 Of A Kind Accessories' },
+    mediaUrls: [
+      {
+        image: '202008/Z/7cb3b42a-dcb4-11ea-90f6-002590aaee66_Z.png',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: 'ColorSwatch/202008/5122b8fa-dcba-11ea-90f9-002590aaee66.png',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+    ],
+    variantMedia: [
+      {
+        sku: '026-BLA-O-16796617860',
+        url: 'ColorSwatch/202008/5122b8fa-dcba-11ea-90f9-002590aaee66.png',
+      },
+    ],
+    variants: [
+      {
+        color: 'BLACK',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '4',
+        sku: '026-BLA-O-16796617860',
+        resalePrice: '6.4',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: '20-pc Assorted Cartoon Reusable Mask',
+    sourceId: 'UHJvZHVjdDoxMjc1MA==',
+    description:
+      '{"time": 1662995227870, "blocks": [{"id": "cqWmV3MIPH", "data": {"text": "20-pc Assorted Cartoon Mouth Reusable Mask "}, "type": "paragraph"}], "version": "2.24.3"}',
+    categoryId: 'Q2F0ZWdvcnk6MTU=',
+    styleNumber: 'LYJKMSK',
+    vendorDetails: { vendorId: '521', vendorName: 'Love and Repeat' },
+    mediaUrls: [
+      {
+        image: '202007/Z/4666631e-c90d-11ea-8b09-002590aaee66_Z.jpg',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: '202007/Z/466a6306-c90d-11ea-8b09-002590aaee66_Z.jpg',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: '202007/Z/4666631f-c90d-11ea-8b09-002590aaee66_Z.jpg',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+    ],
+    variantMedia: [
+      {
+        sku: '20P-MIX-O-16796624430',
+        url: '202007/Z/4666631e-c90d-11ea-8b09-002590aaee66_Z.jpg',
+      },
+    ],
+    variants: [
+      {
+        color: 'MIX COLOR',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '1.85',
+        sku: '20P-MIX-O-16796624430',
+        resalePrice: '2.96',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: '20-pc Assorted Cartoon Reusable Mask',
+    sourceId: 'UHJvZHVjdDoxMjc2MA==',
+    description:
+      '{"time": 1662995227870, "blocks": [{"id": "cqWmV3MIPH", "data": {"text": "20-pc Assorted Cartoon Mouth Reusable Mask "}, "type": "paragraph"}], "version": "2.24.3"}',
+    categoryId: 'Q2F0ZWdvcnk6MTU=',
+    styleNumber: 'LYJKMSK-CT',
+    vendorDetails: { vendorId: '521', vendorName: 'Love and Repeat' },
+    mediaUrls: [
+      {
+        image: '202007/Z/c11a131a-ccf2-11ea-8c7e-002590aaee66_Z.jpg',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: '202007/Z/bb9e572a-ccf2-11ea-8c7e-002590aaee66_Z.jpg',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: '202007/Z/be2a5eee-ccf2-11ea-8c7e-002590aaee66_Z.jpg',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+    ],
+    variantMedia: [
+      {
+        sku: '20P-MIX-O-16796624560',
+        url: '202007/Z/c11a131a-ccf2-11ea-8c7e-002590aaee66_Z.jpg',
+      },
+    ],
+    variants: [
+      {
+        color: 'MIX COLOR',
+        commission: '10',
+        size: 'ONE SIZE',
+        'cost-price': '1.85',
+        sku: '20P-MIX-O-16796624560',
+        resalePrice: '2.96',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: '3-pc Mask Filter Refill Sheet Set',
+    sourceId: 'UHJvZHVjdDoxMjE4Mw==',
+    description:
+      '{"time": 1662995227870, "blocks": [{"id": "cqWmV3MIPH", "data": {"text": "3-pc Mask Filter Refill Sheet Set "}, "type": "paragraph"}], "version": "2.24.3"}',
+    categoryId: 'Q2F0ZWdvcnk6MTU=',
+    styleNumber: 'F169965',
+    vendorDetails: { vendorId: '521', vendorName: 'Love and Repeat' },
+    mediaUrls: [
+      {
+        image: '202007/Z/a663eb4a-c27f-11ea-88db-002590aaee66_Z.jpg',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+      {
+        image: '202007/Z/a6615eac-c27f-11ea-88db-002590aaee66_Z.jpg',
+        type: 'IMAGE',
+        alt: 'alt',
+        oembed_data: {},
+        to_remove: false,
+      },
+    ],
+    variantMedia: [
+      {
+        sku: '3PC-WHI-O-16796599180',
+        url: '202007/Z/a663eb4a-c27f-11ea-88db-002590aaee66_Z.jpg',
+      },
+    ],
+    variants: [
+      {
+        color: 'WHITE',
+        commission: '10',
+        size: 'ONE',
+        'cost-price': '0.85',
+        sku: '3PC-WHI-O-16796599180',
+        resalePrice: '1.36',
+        stock: [
+          {
+            warehouseId:
+              'V2FyZWhvdXNlOjYxYWU5MGU5LWRlNTItNDQ3NC05YzY2LTJkMzk5ZDhmYWIzZg==',
+            quantity: 1000,
+          },
+        ],
+      },
+    ],
+  },
+];
