c822c8d7669db8205adabf5ae86bf793dd1bf2d3
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-04-04 12:39:03 +0500

    Update : sync categories api for frontend

--- src/modules/Product/Product.dto.ts
+++ src/modules/Product/Product.dto.ts
@@ -1,5 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsArray, IsString, ArrayMinSize } from 'class-validator';
+
+export class ImportBulkCategoriesDto {
+  @ApiProperty({
+    required: true,
+    description: 'list of categories you want to sync ',
+    isArray: true,
+  })
+  @IsArray()
+  @ArrayMinSize(1)
+  categoryIds: string[];
+
+  @ApiProperty({ required: true, description: 'b2b shop id of retailer' })
+  @IsString()
+  shopId: string;
+
+  @ApiProperty({
+    required: true,
+    description: 'b2c store id for which products should be added against',
+  })
+  @IsString()
+  storeId: string;
+}
 
 export class AutoSyncDto {
   @ApiProperty({
--- src/modules/Product/services/productMapping/Product.mapping.service.ts
+++ src/modules/Product/services/productMapping/Product.mapping.service.ts
@@ -13,6 +13,7 @@ import {
 import axiosRetry from 'axios-retry';
 import { AutoSyncDto } from '../../Product.dto';
 import { ProductTransformedDto } from '../../transformer/Product.transformer.types';
+import polly from 'polly-js';
 @Injectable()
 export class ProductMappingService {
   private readonly logger = new Logger(ProductMappingService.name);
@@ -22,49 +23,54 @@ export class ProductMappingService {
    * @warn -- this can crete mappings using falsy vales as well , because of how ES stores its documents
    */
   public async saveBulkMappings(mappingsList: ProductMappingsDto[]) {
-    try {
-      if (mappingsList.length == 0) return;
-      const addProductMapping = await axios.post(
-        `${MAPPING_SERVICE_URL}/documents`,
-        JSON.stringify(mappingsList),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer private-${MAPPING_MAPPING_TOKEN}`,
+    return polly()
+      .logger(function (error) {
+        this.logger.error(error);
+      })
+      .waitAndRetry(4)
+      .executeForPromise(async () => {
+        if (mappingsList.length == 0) return;
+        const addProductMapping = await axios.post(
+          `${MAPPING_SERVICE_URL}/documents`,
+          JSON.stringify(mappingsList),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer private-${MAPPING_MAPPING_TOKEN}`,
+            },
           },
-        },
-      );
-      axiosRetry(axios, { retries: 3 });
-      return addProductMapping.data;
-    } catch (error) {
-      this.logger.error(error);
-    }
+        );
+        return addProductMapping.data;
+      });
   }
 
   /**
    * @description -- this method stores mapping for retailer against its category
    */
   public async saveSyncCategoryMapping(mappingData: AutoSyncDto) {
-    try {
-      const mappingObject: SyncCategoryMappingDto = {
-        shr_category_id: mappingData.categoryId,
-        shr_retailer_shop_id: mappingData.shopId,
-      };
-      const addProductMapping = await axios.post(
-        `${AUTO_SYNC_MAPPING_URL}/documents`,
-        JSON.stringify(mappingObject),
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer private-${MAPPING_MAPPING_TOKEN}`,
+    return polly()
+      .logger(function (error) {
+        this.logger.error(error);
+      })
+      .waitAndRetry(4)
+      .executeForPromise(async () => {
+        const mappingObject: SyncCategoryMappingDto = {
+          shr_category_id: mappingData.categoryId,
+          shr_retailer_shop_id: mappingData.shopId,
+        };
+        const addProductMapping = await axios.post(
+          `${AUTO_SYNC_MAPPING_URL}/documents`,
+          JSON.stringify(mappingObject),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer private-${MAPPING_MAPPING_TOKEN}`,
+            },
           },
-        },
-      );
-      axiosRetry(axios, { retries: 3 });
-      return addProductMapping.data;
-    } catch (error) {
-      this.logger.error(error);
-    }
+        );
+        axiosRetry(axios, { retries: 3 });
+        return addProductMapping.data;
+      });
   }
 
   /**
@@ -74,34 +80,37 @@ export class ProductMappingService {
     productsData: ProductTransformedDto[],
     autoSyncInput: AutoSyncDto,
   ) {
-    try {
-      const productIds = productsData.map((product) => {
-        return product.sourceId;
+    return polly()
+      .logger(function (error) {
+        this.logger.error(error);
+      })
+      .waitAndRetry(4)
+      .executeForPromise(async () => {
+        const productIds = productsData.map((product) => {
+          return product.sourceId;
+        });
+        const filters = JSON.stringify({
+          query: '',
+          page: { size: 100 },
+          filters: {
+            all: [
+              {
+                shr_b2b_product_id: productIds,
+              },
+              {
+                retailer_id: autoSyncInput.shopId,
+              },
+            ],
+          },
+        });
+        const getProductsMapping = await axios.post(
+          `${MAPPING_SERVICE_URL}/search`,
+          filters,
+          MAPPING_SERVICE_HEADERS,
+        );
+        axiosRetry(axios, { retries: 3 });
+        return getProductsMapping.data;
       });
-      const filters = JSON.stringify({
-        query: '',
-        page: { size: 100 },
-        filters: {
-          all: [
-            {
-              shr_b2b_product_id: productIds,
-            },
-            {
-              retailer_id: autoSyncInput.shopId,
-            },
-          ],
-        },
-      });
-      const getProductsMapping = await axios.post(
-        `${MAPPING_SERVICE_URL}/search`,
-        filters,
-        MAPPING_SERVICE_HEADERS,
-      );
-      axiosRetry(axios, { retries: 3 });
-      return getProductsMapping.data;
-    } catch (error) {
-      this.logger.error(error);
-    }
   }
 
   /**
@@ -131,6 +140,7 @@ export class ProductMappingService {
       axiosRetry(axios, { retries: 3 });
       return getSyncedCategoriesMapping.data;
     } catch (error) {
+      console.log(error);
       this.logger.error(error);
     }
   }
@@ -142,24 +152,28 @@ export class ProductMappingService {
     autoSyncInput: AutoSyncDto,
     productsList: ProductTransformedDto[],
   ) {
-    try {
-      const [productMappings, categoryMappings] = await Promise.all([
-        this.getProductsMapping(productsList, autoSyncInput),
-        this.getSyncCategoryMappings(autoSyncInput),
-      ]);
-      if (categoryMappings.results.length == 0) return [];
-      return productsList.filter((product) => {
-        if (
-          productMappings.results.some(
-            (mapping) => mapping.shr_b2b_product_id.raw === product.sourceId,
-          )
-        ) {
-          return;
-        }
-        return product;
+    return polly()
+      .logger(function (error) {
+        this.logger.error(error);
+      })
+      .waitAndRetry(4)
+      .executeForPromise(async () => {
+        const [productMappings, categoryMappings] = await Promise.all([
+          this.getProductsMapping(productsList, autoSyncInput),
+          this.getSyncCategoryMappings(autoSyncInput),
+        ]);
+        if (categoryMappings?.results?.length == 0) return [];
+        return productsList.filter((product) => {
+          if (
+            productMappings?.results?.some(
+              (mapping) =>
+                mapping?.shr_b2b_product_id?.raw === product.sourceId,
+            )
+          ) {
+            return;
+          }
+          return product;
+        });
       });
-    } catch (error) {
-      this.logger.error(error);
-    }
   }
 }
