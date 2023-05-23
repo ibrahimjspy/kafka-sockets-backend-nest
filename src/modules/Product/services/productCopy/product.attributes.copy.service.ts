0bfaa1bc7ac8df3cd95ff6d7205f3a26937f5819
ibrahimjspy <ibrahimsajidjs@gmail.com>
2023-05-23 23:48:53 +0500

    Update: added logging for product create methods

--- src/modules/Product/services/productCopy/product.attributes.copy.service.ts
+++ src/modules/Product/services/productCopy/product.attributes.copy.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { In, Repository } from 'typeorm';
 import { AttributeAssignedProductAttribute } from 'src/database/destination/product/attributes/assignment';
@@ -9,6 +9,8 @@ import { AttributeAttributeValue } from 'src/database/destination/attributes';
 
 @Injectable()
 export class ProductAttributeCopyService {
+  private readonly logger = new Logger(ProductAttributeCopyService.name);
+
   constructor(
     @InjectRepository(AttributeAssignedProductAttribute)
     private assignedProductAttributeRepository: Repository<AttributeAssignedProductAttribute>,
@@ -25,10 +27,23 @@ export class ProductAttributeCopyService {
    * @returns A Promise that resolves when the attribute copying is complete.
    */
   async copyProductAttributes(copiedProducts: ProductProduct[]): Promise<void> {
-    const copiedAssignments = await this.copyProductAttributeAssignments(
-      copiedProducts,
-    );
-    await this.copyAssignedProductAttributeValues(copiedAssignments);
+    try {
+      this.logger.log('Copying product attributes for copied products');
+
+      const copiedAssignments = await this.copyProductAttributeAssignments(
+        copiedProducts,
+      );
+
+      await this.copyAssignedProductAttributeValues(copiedAssignments);
+
+      this.logger.log('Product attribute copying is complete');
+    } catch (error) {
+      this.logger.error(
+        'An error occurred while copying product attributes for copied products',
+        error,
+      );
+      throw error;
+    }
   }
 
   /**
@@ -39,30 +54,51 @@ export class ProductAttributeCopyService {
   private async copyProductAttributeAssignments(
     copiedProducts: ProductProduct[],
   ): Promise<Map<number, number>> {
-    const copiedAssignments = [];
-    const productIds =
-      this.transformerService.getMasterProducts(copiedProducts);
-    const productMapping =
-      this.transformerService.getProductMapping(copiedProducts);
-    const masterAssignments =
-      await this.assignedProductAttributeRepository.find({
-        where: { productId: In(productIds) },
-      });
+    try {
+      this.logger.log('Copying attribute assignments for copied products');
 
-    for (const assignment of masterAssignments) {
-      const copyAssignment = { ...assignment };
-      delete copyAssignment.id;
-      copyAssignment.productId = productMapping.get(assignment.productId);
-      copiedAssignments.push(copyAssignment);
+      const copiedAssignments = [];
+      const productIds =
+        this.transformerService.getMasterProducts(copiedProducts);
+      const productMapping =
+        this.transformerService.getProductMapping(copiedProducts);
+      const masterAssignments =
+        await this.assignedProductAttributeRepository.find({
+          where: { productId: In(productIds) },
+        });
+
+      this.logger.log(
+        `Retrieved ${masterAssignments.length} master attribute assignments`,
+      );
+
+      for (const assignment of masterAssignments) {
+        const copyAssignment = { ...assignment };
+        delete copyAssignment.id;
+        copyAssignment.productId = productMapping.get(assignment.productId);
+        copiedAssignments.push(copyAssignment);
+      }
+
+      // Save the copied assignments
+      const copiedAssignmentsResponse =
+        await this.assignedProductAttributeRepository.save(copiedAssignments);
+
+      // Generate mapping between original and copied assignment IDs
+      const assignmentIdsMapping =
+        this.transformerService.getAssignmentIdsMapping(
+          copiedProducts,
+          masterAssignments,
+          copiedAssignmentsResponse,
+        );
+
+      this.logger.log('Attribute assignments copying is complete');
+      return assignmentIdsMapping;
+    } catch (error) {
+      this.logger.error(
+        'An error occurred while copying attribute assignments for copied products',
+        error,
+      );
+      throw error;
     }
-
-    const copiedAssignmentsResponse =
-      await this.assignedProductAttributeRepository.save(copiedAssignments);
-    return this.transformerService.getAssignmentIdsMapping(
-      copiedProducts,
-      masterAssignments,
-      copiedAssignmentsResponse,
-    );
   }
 
   /**
@@ -73,30 +109,49 @@ export class ProductAttributeCopyService {
   private async copyAssignedProductAttributeValues(
     assignmentMapping: Map<number, number>,
   ): Promise<void> {
-    const assignments = Array.from(assignmentMapping.keys());
-    const copyAttributeAssignmentValues = [];
-    const attributeValuesAssigned =
-      await this.assignedProductAttributeValueRepository.find({
-        where: { assignmentId: In(assignments) },
-      });
-    const valueIds = attributeValuesAssigned.map((value) => value.valueId);
-    const valuesMapping = await this.copyAttributeValues(valueIds);
-
-    for (const attributeAssignedValue of attributeValuesAssigned) {
-      const copyAttributeValueAssigned = { ...attributeAssignedValue };
-      const assignmentId = assignmentMapping.get(
-        attributeAssignedValue.assignmentId,
+    try {
+      this.logger.log(
+        'Copying assigned attribute values for copied attribute assignments',
       );
-      copyAttributeValueAssigned.assignmentId = assignmentId;
-      const valueId = valuesMapping.get(attributeAssignedValue.valueId);
-      copyAttributeValueAssigned.valueId = valueId;
-      delete copyAttributeValueAssigned.id;
-      copyAttributeAssignmentValues.push(copyAttributeValueAssigned);
-    }
 
-    await this.assignedProductAttributeValueRepository.save(
-      copyAttributeAssignmentValues,
-    );
+      const assignments = Array.from(assignmentMapping.keys());
+      const copyAttributeAssignmentValues = [];
+      const attributeValuesAssigned =
+        await this.assignedProductAttributeValueRepository.find({
+          where: { assignmentId: In(assignments) },
+        });
+      const valueIds = attributeValuesAssigned.map((value) => value.valueId);
+      const valuesMapping = await this.copyAttributeValues(valueIds);
+
+      this.logger.log(
+        `Retrieved ${attributeValuesAssigned.length} assigned attribute values`,
+      );
+
+      for (const attributeAssignedValue of attributeValuesAssigned) {
+        const copyAttributeValueAssigned = { ...attributeAssignedValue };
+        const assignmentId = assignmentMapping.get(
+          attributeAssignedValue.assignmentId,
+        );
+        copyAttributeValueAssigned.assignmentId = assignmentId;
+        const valueId = valuesMapping.get(attributeAssignedValue.valueId);
+        copyAttributeValueAssigned.valueId = valueId;
+        delete copyAttributeValueAssigned.id;
+        copyAttributeAssignmentValues.push(copyAttributeValueAssigned);
+      }
+
+      // Save the copied assigned attribute values
+      await this.assignedProductAttributeValueRepository.save(
+        copyAttributeAssignmentValues,
+      );
+
+      this.logger.log('Assigned attribute values copying is complete');
+    } catch (error) {
+      this.logger.error(
+        'An error occurred while copying assigned attribute values for copied attribute assignments',
+        error,
+      );
+      throw error;
+    }
   }
 
   /**
@@ -107,28 +162,46 @@ export class ProductAttributeCopyService {
   private async copyAttributeValues(
     valueIds: number[],
   ): Promise<Map<number, number>> {
-    const copiedValues = [];
-    const attributeValues = await this.attributeValueRepository.find({
-      where: { id: In(valueIds) },
-    });
-    const attributeValuesSlugMapping: Map<string, number> = new Map();
+    try {
+      this.logger.log('Copying attribute values');
 
-    for (const attributeValue of attributeValues) {
-      const copyAttributeValue = { ...attributeValue };
-      const slug = this.transformerService.getProductSlug({
-        slug: attributeValue.slug,
-      } as ProductProduct);
-      copyAttributeValue.slug = slug;
-      attributeValuesSlugMapping.set(slug, attributeValue.id);
-      delete copyAttributeValue.id;
-      copiedValues.push(copyAttributeValue);
+      const copiedValues = [];
+      const attributeValues = await this.attributeValueRepository.find({
+        where: { id: In(valueIds) },
+      });
+      const attributeValuesSlugMapping: Map<string, number> = new Map();
+
+      this.logger.log(`Retrieved ${attributeValues.length} attribute values`);
+
+      for (const attributeValue of attributeValues) {
+        const copyAttributeValue = { ...attributeValue };
+        const slug = this.transformerService.getProductSlug({
+          slug: attributeValue.slug,
+        } as ProductProduct);
+        copyAttributeValue.slug = slug;
+        attributeValuesSlugMapping.set(slug, attributeValue.id);
+        delete copyAttributeValue.id;
+        copiedValues.push(copyAttributeValue);
+      }
+
+      // Save the copied attribute values
+      const copiedValuesResponse: AttributeAttributeValue[] =
+        await this.attributeValueRepository.save(copiedValues);
+
+      // Generate mapping between original and copied attribute value IDs
+      const valuesMapping = this.transformerService.getValuesMapping(
+        attributeValuesSlugMapping,
+        copiedValuesResponse,
+      );
+
+      this.logger.log('Attribute values copying is complete');
+      return valuesMapping;
+    } catch (error) {
+      this.logger.error(
+        'An error occurred while copying attribute values',
+        error,
+      );
+      throw error;
     }
-
-    const copiedValuesResponse: AttributeAttributeValue[] =
-      await this.attributeValueRepository.save(copiedValues);
-    return this.transformerService.getValuesMapping(
-      attributeValuesSlugMapping,
-      copiedValuesResponse,
-    );
   }
 }
--- src/modules/Product/services/productCopy/product.variant.attribute.copy.service.ts
+++ src/modules/Product/services/productCopy/product.variant.attribute.copy.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { In, Repository } from 'typeorm';
 import { ProductProduct } from 'src/database/destination/product/product';
@@ -10,6 +10,8 @@ import { AttributeAssignedVariantAttributeValue } from 'src/database/destination
 
 @Injectable()
 export class ProductVariantAttributeCopyService {
+  private readonly logger = new Logger(ProductVariantAttributeCopyService.name);
+
   constructor(
     @InjectRepository(AttributeAssignedVariantAttribute)
     private assignedVariantAttributeRepository: Repository<AttributeAssignedVariantAttribute>,
@@ -28,10 +30,22 @@ export class ProductVariantAttributeCopyService {
   async copyVariantAttributes(
     copiedVariants: ProductProductVariant[],
   ): Promise<void> {
-    const copiedAssignments = await this.copyProductAttributeAssignments(
-      copiedVariants,
-    );
-    await this.copyAssignedVariantAttributeValues(copiedAssignments);
+    try {
+      this.logger.log('Copying variant attributes for copied variants');
+
+      const copiedAssignments = await this.copyProductAttributeAssignments(
+        copiedVariants,
+      );
+      await this.copyAssignedVariantAttributeValues(copiedAssignments);
+
+      this.logger.log('Variant attribute copying is complete');
+    } catch (error) {
+      this.logger.error(
+        'An error occurred while copying variant attributes for copied variants',
+        error,
+      );
+      throw error;
+    }
   }
 
   /**
@@ -42,30 +56,51 @@ export class ProductVariantAttributeCopyService {
   private async copyProductAttributeAssignments(
     copiedVariants: ProductProductVariant[],
   ): Promise<Map<number, number>> {
-    const copiedAssignments: AttributeAssignedVariantAttribute[] = [];
-    const variantIds =
-      this.transformerService.getMasterProductVariants(copiedVariants);
-    const variantsMapping =
-      this.transformerService.getProductVariantMapping(copiedVariants);
-    const masterAssignments =
-      await this.assignedVariantAttributeRepository.find({
-        where: { variantId: In(variantIds) },
-      });
+    try {
+      this.logger.log('Copying attribute assignments for copied variants');
 
-    for (const assignment of masterAssignments) {
-      const copyAssignment = { ...assignment };
-      delete copyAssignment.id;
-      copyAssignment.variantId = variantsMapping.get(assignment.variantId);
-      copiedAssignments.push(copyAssignment);
+      const copiedAssignments: AttributeAssignedVariantAttribute[] = [];
+      const variantIds =
+        this.transformerService.getMasterProductVariants(copiedVariants);
+      const variantsMapping =
+        this.transformerService.getProductVariantMapping(copiedVariants);
+      const masterAssignments =
+        await this.assignedVariantAttributeRepository.find({
+          where: { variantId: In(variantIds) },
+        });
+
+      this.logger.log(
+        `Retrieved ${masterAssignments.length} attribute assignments`,
+      );
+
+      for (const assignment of masterAssignments) {
+        const copyAssignment = { ...assignment };
+        delete copyAssignment.id;
+        copyAssignment.variantId = variantsMapping.get(assignment.variantId);
+        copiedAssignments.push(copyAssignment);
+      }
+
+      // Save the copied attribute assignments
+      const copiedAssignmentsResponse =
+        await this.assignedVariantAttributeRepository.save(copiedAssignments);
+
+      // Generate mapping between original and copied assignment IDs
+      const assignmentMapping =
+        this.transformerService.getVariantAssignmentIdsMapping(
+          copiedVariants,
+          masterAssignments,
+          copiedAssignmentsResponse,
+        );
+
+      this.logger.log('Attribute assignments copying is complete');
+      return assignmentMapping;
+    } catch (error) {
+      this.logger.error(
+        'An error occurred while copying attribute assignments for copied variants',
+        error,
+      );
+      throw error;
     }
-
-    const copiedAssignmentsResponse =
-      await this.assignedVariantAttributeRepository.save(copiedAssignments);
-    return this.transformerService.getVariantAssignmentIdsMapping(
-      copiedVariants,
-      masterAssignments,
-      copiedAssignmentsResponse,
-    );
   }
 
   /**
@@ -76,31 +111,50 @@ export class ProductVariantAttributeCopyService {
   private async copyAssignedVariantAttributeValues(
     assignmentMapping: Map<number, number>,
   ): Promise<void> {
-    const assignments = Array.from(assignmentMapping.keys());
-    const copyAttributeAssignmentValues: AttributeAssignedVariantAttributeValue[] =
-      [];
-    const attributeValuesAssigned =
-      await this.assignedVariantAttributeValueRepository.find({
-        where: { assignmentId: In(assignments) },
-      });
-    const valueIds = attributeValuesAssigned.map((value) => value.valueId);
-    const valuesMapping = await this.copyAttributeValues(valueIds);
-
-    for (const attributeAssignedValue of attributeValuesAssigned) {
-      const copyAttributeValueAssigned = { ...attributeAssignedValue };
-      const assignmentId = assignmentMapping.get(
-        attributeAssignedValue.assignmentId,
+    try {
+      this.logger.log(
+        'Copying assigned attribute values for copied attribute assignments',
       );
-      copyAttributeValueAssigned.assignmentId = assignmentId;
-      const valueId = valuesMapping.get(attributeAssignedValue.valueId);
-      copyAttributeValueAssigned.valueId = valueId;
-      delete copyAttributeValueAssigned.id;
-      copyAttributeAssignmentValues.push(copyAttributeValueAssigned);
-    }
 
-    await this.assignedVariantAttributeValueRepository.save(
-      copyAttributeAssignmentValues,
-    );
+      const assignments = Array.from(assignmentMapping.keys());
+      const copyAttributeAssignmentValues: AttributeAssignedVariantAttributeValue[] =
+        [];
+      const attributeValuesAssigned =
+        await this.assignedVariantAttributeValueRepository.find({
+          where: { assignmentId: In(assignments) },
+        });
+      const valueIds = attributeValuesAssigned.map((value) => value.valueId);
+      const valuesMapping = await this.copyAttributeValues(valueIds);
+
+      this.logger.log(
+        `Retrieved ${attributeValuesAssigned.length} assigned attribute values`,
+      );
+
+      for (const attributeAssignedValue of attributeValuesAssigned) {
+        const copyAttributeValueAssigned = { ...attributeAssignedValue };
+        const assignmentId = assignmentMapping.get(
+          attributeAssignedValue.assignmentId,
+        );
+        copyAttributeValueAssigned.assignmentId = assignmentId;
+        const valueId = valuesMapping.get(attributeAssignedValue.valueId);
+        copyAttributeValueAssigned.valueId = valueId;
+        delete copyAttributeValueAssigned.id;
+        copyAttributeAssignmentValues.push(copyAttributeValueAssigned);
+      }
+
+      // Save the copied assigned attribute values
+      await this.assignedVariantAttributeValueRepository.save(
+        copyAttributeAssignmentValues,
+      );
+
+      this.logger.log('Assigned attribute values copying is complete');
+    } catch (error) {
+      this.logger.error(
+        'An error occurred while copying assigned attribute values for copied attribute assignments',
+        error,
+      );
+      throw error;
+    }
   }
 
   /**
@@ -111,29 +165,47 @@ export class ProductVariantAttributeCopyService {
   private async copyAttributeValues(
     valueIds: number[],
   ): Promise<Map<number, number>> {
-    const copiedValues: AttributeAttributeValue[] = [];
-    const attributeValues = await this.attributeValueRepository.find({
-      where: { id: In(valueIds) },
-    });
-    const attributeValuesSlugMapping = new Map();
+    try {
+      this.logger.log('Copying attribute values');
 
-    for (const attributeValue of attributeValues) {
-      const copyAttributeValue = { ...attributeValue };
-      const slug = this.transformerService.getProductSlug({
-        slug: attributeValue.slug,
-      } as ProductProduct);
-      copyAttributeValue.slug = slug;
-      attributeValuesSlugMapping.set(slug, attributeValue.id);
-      delete copyAttributeValue.id;
-      copiedValues.push(copyAttributeValue);
+      const copiedValues: AttributeAttributeValue[] = [];
+      const attributeValues = await this.attributeValueRepository.find({
+        where: { id: In(valueIds) },
+      });
+      const attributeValuesSlugMapping = new Map();
+
+      this.logger.log(`Retrieved ${attributeValues.length} attribute values`);
+
+      for (const attributeValue of attributeValues) {
+        const copyAttributeValue = { ...attributeValue };
+        const slug = this.transformerService.getProductSlug({
+          slug: attributeValue.slug,
+        } as ProductProduct);
+        copyAttributeValue.slug = slug;
+        attributeValuesSlugMapping.set(slug, attributeValue.id);
+        delete copyAttributeValue.id;
+        copiedValues.push(copyAttributeValue);
+      }
+
+      // Save the copied attribute values
+      const copiedValuesResponse = await this.attributeValueRepository.save(
+        copiedValues,
+      );
+
+      // Generate mapping between original and copied attribute value IDs
+      const valuesMapping = this.transformerService.getValuesMapping(
+        attributeValuesSlugMapping,
+        copiedValuesResponse,
+      );
+
+      this.logger.log('Attribute values copying is complete');
+      return valuesMapping;
+    } catch (error) {
+      this.logger.error(
+        'An error occurred while copying attribute values',
+        error,
+      );
+      throw error;
     }
-
-    const copiedValuesResponse = await this.attributeValueRepository.save(
-      copiedValues,
-    );
-    return this.transformerService.getValuesMapping(
-      attributeValuesSlugMapping,
-      copiedValuesResponse,
-    );
   }
 }
